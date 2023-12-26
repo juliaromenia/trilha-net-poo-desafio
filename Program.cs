71612b84c8e3ceef4cd544b7ef5cf92b277eb027
Julia Romenia <juliarimeniag1@gmail.com>
2023-12-25 22:37:57 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Celular Nokia: ");
+Smartphone nokia = new Nokia(numero: "99222-2222", modelo: "Nokia4", imei: "010101", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("Celular Iphone: ");
+Smartphone iphone = new Iphone(numero: "98111-1111", modelo: "Iphone11", imei: "101010", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
